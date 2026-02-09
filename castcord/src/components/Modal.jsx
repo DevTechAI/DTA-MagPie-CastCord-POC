8cc4e2a48145e9b6200f8d06a4ae722372feb3f1
Nithin Bagepalli <nithin.bagepalli@devtechai.org>
2026-02-09 02:11:31 -0800

    removed the json file from the thank you page

--- castcord/src/components/Modal.jsx
+++ castcord/src/components/Modal.jsx
@@ -8,24 +8,6 @@ export default function Modal({ data, onClose }) {
         <h3>{thankYouHeading}</h3>
         <p className="modal-message">Your application has been submitted successfully.</p>
 
-        {data?.candidate_photo_url && (
-          <div className="modal-photo">
-            <img src={data.candidate_photo_url} alt="Your photo" />
-          </div>
-        )}
-        {(data?.youtube_link || data?.portfolio_link) && (
-          <div className="modal-links">
-            {data.youtube_link && (
-              <a href={data.youtube_link} target="_blank" rel="noopener noreferrer">YouTube link</a>
-            )}
-            {data.portfolio_link && (
-              <a href={data.portfolio_link} target="_blank" rel="noopener noreferrer">Portfolio link</a>
-            )}
-          </div>
-        )}
-
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-
         <button className="btn btn-secondary" onClick={onClose}>
           Close
         </button>
